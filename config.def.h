62d89f31ef9080fe62703773c43baec2e98366d7
Idlidev <idliyout@gmail.com>
2022-01-06 11:46:51 +0530

    added alpha patch

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetbrainsMono:size=12"
@@ -13,6 +14,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#D8DEE9", "#5E81AC" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 12;
 static unsigned int columns    = 1;
