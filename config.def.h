e1b83dab958e2d8b49d5cdb0f65909fcec5c4bd3
Idlidev <idliyout@gmail.com>
2022-01-09 15:45:14 +0530

    modified colours

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
 	[SchemeSel] = { "#D8DEE9", "#5E81AC" },
 	[SchemeOut] = { "#D8DEE9", "#2E3440" },
-	[SchemeSelHighlight] = { "#88C0D0", "#3B4252" },
+	[SchemeSelHighlight] = { "#4C566A", "#81A1C1" },
 	[SchemeNormHighlight] = { "#88C0D0", "#3B4252" },
 	[SchemeHp] = { "#D8DEE9", "#88C0D0" }
 };
