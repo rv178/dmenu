f6c86499966c8866e332a3c0d0aa4f380fb9838a
Idlidev <idliyout@gmail.com>
2022-01-11 16:12:47 +0530

    decreased font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetbrainsMono:size=12"
+	"JetbrainsMono:size=11"
 };
 
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
